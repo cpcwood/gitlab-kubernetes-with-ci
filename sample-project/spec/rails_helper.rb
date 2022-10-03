6a24bfba9a5806e4bf0a0406964882ed8eb4c46e
Chris Wood <chris.wood@thebeansgroup.com>
2022-10-03 19:43:08 +0100

    lint

--- sample-project/spec/rails_helper.rb
+++ sample-project/spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'rails-controller-testing'
 Rails::Controller::Testing.install
 # Add additional requires below this line. Rails is not loaded until this point!
 
-Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
